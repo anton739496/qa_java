d0bdf6c12aa39a8256a5af95c3cb7f8d0d5b31d3
Anton Tabaev <tonygrander@gmail.com>
2024-06-22 08:06:57 +0100

    Исправленны тесты:ассерты,моки,параметризация

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,20 +1,54 @@
 package com.example;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(Parameterized.class)
 public class CatTest {
-    Feline feline = new Feline();
+
     @Mock
-    Cat cat = new Cat(feline);
+    Feline feline;
+
+    Cat cat;
+
+    private final List<String> expectedFood;
+
+    public CatTest(List<String> expectedFood) {
+        this.expectedFood = expectedFood;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { List.of("Животные", "Птицы", "Рыба") }
+        });
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        cat = new Cat(feline);
+    }
+
     @Test
     public void sound() {
         Assert.assertEquals("Мяу", cat.getSound());
     }
+
     @Test
     public void eatMeat() throws Exception {
-        Assert.assertEquals(cat.getFood(), List.of("Животные", "Птицы", "Рыба"));
+        when(feline.eatMeat()).thenReturn(expectedFood);
+        Assert.assertEquals(expectedFood, cat.getFood());
     }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -2,42 +2,65 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
 import java.util.List;
 
+@RunWith(Parameterized.class)
 public class LionTest {
-    Feline feline = new Feline();
-    @Test
-    public void setMale() throws Exception {
-        Lion lion = new Lion("Самец", feline);
-        Assert.assertTrue(lion.doesHaveMane());
+    @Mock
+    Feline feline;
+
+    private final String sex;
+    private final boolean expectedMane;
+
+    public LionTest(String sex, boolean expectedMane) {
+        this.sex = sex;
+        this.expectedMane = expectedMane;
+    }
+
+    @Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        });
     }
 
     @Test
-    public void setFemale() throws Exception {
-        Lion lion = new Lion("Самка", feline);
-        Assert.assertFalse(lion.doesHaveMane());
+    public void setMaleOrFemale() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        Assert.assertEquals(expectedMane, lion.doesHaveMane());
     }
 
     @Test
     public void testException() throws Exception {
         Throwable ex = Assert.assertThrows(
-                new Exception("Используйте допустимые значения пола животного - самец или самка").getClass(),
-                ()->{Lion lion = new Lion("abcd", feline);}
+                Exception.class,
+                () -> { Lion lion = new Lion("abcd", feline); }
         );
+        Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", ex.getMessage());
     }
+
     @Test
     public void eatMeat() throws Exception {
         Feline mockFeline = Mockito.mock(Feline.class);
         Mockito.when(mockFeline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         Lion lion = new Lion("Самец", mockFeline);
-        Assert.assertEquals(lion.getFood(), List.of("Животные", "Птицы", "Рыба"));
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
     }
+
     @Test
     public void kittens() throws Exception {
-        int kittens = (new Lion("Самец", feline)).getKittens();
-        Assert.assertEquals(1, kittens);
+        Feline feline = Mockito.mock(Feline.class);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        Lion lion = new Lion("Самец", feline);
+        Assert.assertEquals(1, lion.getKittens());
     }
 }
